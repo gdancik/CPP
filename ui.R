5306e7c48bf2ac1e8ac9506ade03adf13504db64
Garrett Dancik <garrett.dancik@gmail.com>
2017-10-26 21:09:59 -0400

    Sort results in decreasing order and added Please Wait bar...

--- ui.R
+++ ui.R
@@ -38,7 +38,13 @@ shinyUI(
     dashboardBody(
       
       fluidRow(
-        
+
+      conditionalPanel(condition="$('html').hasClass('shiny-busy')",
+             HTML("<br><br>"),
+             HTML("<div class=\"progress\" style=\"height:25px !important\"><div class=\"progress-bar progress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:100%\">
+                                                          <span id=\"bar-text\"><b><font size=\"+1.5\">Loading, please wait...</font></b></span></div></div>")
+      ), 
+
         bsModal("clusterModal", "Document clustering", "btnViewClusters", 
                 plotOutput("clusters"),
                 size = "large"
